400397772368218f6caf00defa50269a5ea5bc10
Phuoc Nguyen <ThienPhuoc@Phuocs-MacBook-Pro.local>
2017-05-17 09:12:59 +0700

    upload

--- book.js
+++ book.js
@@ -1,7 +1,7 @@
 // JavaScript Document
-var app = angular.module('myApp', ['ngRoute']);	
-	app.config(function($routeProvider) {
-  	$routeProvider
+var app = angular.module('myApp', ['ngRoute','ngMaterial','ngMessages','textAngular']);	
+app.config(function($routeProvider) {
+	$routeProvider
 
   .when('/', {
     templateUrl : 'pages/home.html',
@@ -10,67 +10,136 @@ var app = angular.module('myApp', ['ngRoute']);
 
   .when('/bookAll', {
     templateUrl : 'pages/bookAll.html',
-    controller  : 'BookAllController'
+    controller  : 'HomeController'
+  })
+	 .when('/bookAdd', {
+    templateUrl : 'pages/bookAdd.html',
+    controller  : 'HomeController'
   })
 
-  .when('/bookAdd', {
-    templateUrl : 'pages/bookAdd.html',
-    controller  : 'BookAddController'
+	.when('/bookByGenres/:id', {
+    templateUrl : 'pages/bookByGenres.html',
+    controller  : 'HomeController',
   })
-	
-  .when('/bookDetail', {
+
+  .when('/bookDetail/:id', {
     templateUrl : 'pages/bookDetail.html',
-    controller  : 'BookDetailController'
+    controller  : 'HomeController'
   })
-	
+	.when('/bookAuthor/:name', {
+    templateUrl : 'pages/bookAuthor.html',
+    controller  : 'HomeController'
+  })
+	.when('/bookSearch/:text', {
+    templateUrl : 'pages/bookSearch.html',
+    controller  : 'HomeController'
+  })
 	.when('/Cart', {
     templateUrl : 'pages/Cart.html',
     controller  : 'HomeController'
   })
-	
+	.when('/Info', {
+    templateUrl : 'pages/Information.html',
+    controller  : 'HomeController'
+  })
+	.when('/editBook/:id', {
+    templateUrl : 'pages/editBook.html',
+    controller  : 'HomeController'
+  })
   .otherwise({redirectTo: '/'});
 });
+
+app.controller('HomeController', function($scope, $http,$location, $routeParams) {
 	
-app.controller('HomeController', function($scope, $http) {
-    $http.get("https://green-web-bookstore.herokuapp.com/api/books").then(function(response) {
-        $scope.books = response.data;
-    });
+	$scope.getBanners = function() {
+        $http.get("https://green-web-bookstore.herokuapp.com/api/banners").success(function(response) {
+            $scope.banners = response;
+        });
+      }
+	
+	$scope.getBooks = function() {
+        $http.get("https://green-web-bookstore.herokuapp.com/api/books").success(function(response) {
+            $scope.books = response;
+        });
+      }
+  
+   $scope.getBook = function() {
+        var id = $routeParams.id;
+        $http.get("https://green-web-bookstore.herokuapp.com/api/books/" + id).success(function(response) {
+            $scope.book = response;
+		});
+    }
+   
+   $scope.getGenres = function() {
+        $http.get("https://green-web-bookstore.herokuapp.com/api/genres").success(function(response) {
+            $scope.genres = response;
+        });
+    }
+   
+   $scope.getBookByGenres = function() {
+        var id = $routeParams.id;
+	   $http.get("https://green-web-bookstore.herokuapp.com/api/books/genre/" + id).success(function(response) {
+           $scope.books = response;
+        });
+    }
+  
+   $scope.getBookSearch = function() {
+        $scope.text = $routeParams.text;
+        $http.get("https://green-web-bookstore.herokuapp.com/api/books/search/" + $scope.text).success(function(response) {
+            $scope.books = response;
+        });
+    }
+   
+   $scope.getBookByAuthor = function() {
+       $scope.text = $routeParams.name;
+	   $http.get("https://green-web-bookstore.herokuapp.com/api/books/author/" + $scope.text).success(function(response) {
+            $scope.books = response;
+        });
+    }
+  
+   
+   $scope.addBook = function() {
+        console.log($scope.book);
+        $http.post("https://green-web-bookstore.herokuapp.com/api/books/", $scope.book).success(function(response) {
+            window.location.href = '#/books';
+        });
+    }
+
+    $scope.updateBook = function() {
+        var id = $routeParams.id;
+        $http.put("https://green-web-bookstore.herokuapp.com/api/books/" + id, $scope.book).success(function(response) {
+            window.location.href = '#/books/' + $routeParams.id;
+        });
+    }
+
+    $scope.removeBook = function(id) {
+        $http.delete("https://green-web-bookstore.herokuapp.com/api/books/" + id).success(function(response) {
+            window.location.href = '#/books';
+        });
+    }
+
+   
 });
 
-app.controller('BookAllController', function($scope, $http) {
-    $http.get("https://green-web-bookstore.herokuapp.com/api/books").then(function(response) {
-        $scope.books = response.data;
-    });
-});
-
-app.controller('BookAddController', function($scope, $http) {
-    $http.get("https://green-web-bookstore.herokuapp.com/api/books").then(function(response) {
-        $scope.books = response.data;
-    });
-});
-
-app.controller('BookDetailController', function($scope, $http) {
-    $http.get("https://green-web-bookstore.herokuapp.com/api/books").then(function(response) {
-        $scope.books = response.data;
-    });
-});
-
-app.controller('CategorylController', function($scope, $http) {
-    $http.get("http://green-web-bookstore.herokuapp.com/api/genres").then(function(response) {
-        $scope.genres = response.data;
-    });
-});
 
 jQuery.noConflict();
 jQuery(document).ready(function(){
 
 //General
 	jQuery('.remove').click(function(){ 
-		jQuery(this).parents('li.drop-cart').remove()
+		jQuery('.remove').focus();
+		jQuery(this).parents('li.drop-cart').remove();
 		});
 	
 //	Rating Star
 	jQuery('.star').raty({ score: 3 });
-//	Nut upload hinh trong Add Book
+	
+//	Enter
+	jQuery('.nav-input-search').keypress(function(event){
+  		if (event.keyCode == '13') {
+    	jQuery('.nav-search-btn>a').click();
+  		}
+	});
+	
 	
 });
